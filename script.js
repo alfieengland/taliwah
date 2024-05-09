cb46dbf33a7c0ca3940e26c46ef505dbb3df78c5
Alfie England <alfieengland@live.co.uk>
2024-05-09 20:34:22 +0100

    Cinematic Bar Movement on Homepage Load

--- script.js
+++ script.js
@@ -3,7 +3,20 @@ function afterPageLoads() {
 
   setTimeout(() => {
     logoText.style.opacity = "1";
-  },700)
+  },1500)
+
+  var logoText = document.getElementById('logo-text');
+  var rowTop = document.getElementById('row-top');
+  var rowMiddle = document.getElementById('row-middle');
+  var rowBottom = document.getElementById('row-bottom');
+  var canvas = document.getElementById('canvas');
+
+  setTimeout(() => {
+    rowTop.style.height = "20vh";
+    rowMiddle.style.height = "60vh";
+    rowBottom.style.height = "20vh";
+    canvas.style.height = "60vh";
+  },1400)
 }
 
 /* Runs the function after the page has fully loaded */
